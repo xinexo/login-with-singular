47593f1b580a0dfc686bf45ddf6a1f471183dba9
t <hello@singular.live>
2019-08-30 14:36:35 +0700

    improvement: set server url

--- server/router.js
+++ server/router.js
@@ -1,11 +1,16 @@
 var superagent = require('superagent');
-var SINGULAR_API_SERVER = 'http://localhost:3000/apiv1';
+
+var SINGULAR_SERVER = 'https://singular-staging4.herokuapp.com';
+var CLIENT_ID = 'f6fce93827c5f8b2684ac18ffcbe0f6734eb7daa7927590abba1aeb78199267c1e6e380500b322bfa4647f2e2ebb84d3';
+var CLIENT_SECRET = '358fbdbd5b60313a3946eb6efd788661f3be1afded6f7391d465832327fa207a17d7d1ea1279486055433847612e3838';
 
 module.exports = function (app) {
 
   // Startup page
   app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {
+      singularEndpoint: SINGULAR_SERVER + '/oauth/logindialog?client_id=' + CLIENT_ID
+    });
   });
 
   // Callback
@@ -15,27 +20,27 @@ module.exports = function (app) {
       return;
     }
 
-    superagent.post("http://localhost:3000/oauth/accesstoken")
+    superagent.post(SINGULAR_SERVER + '/oauth/accesstoken')
       .send({
         code: req.query.code,
-        client_id: 'f6fce93827c5f8b2684ac18ffcbe0f6734eb7daa7927590abba1aeb78199267c1e6e380500b322bfa4647f2e2ebb84d3',
-        client_secret: '358fbdbd5b60313a3946eb6efd788661f3be1afded6f7391d465832327fa207a17d7d1ea1279486055433847612e3838'
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET
       })
       .end(function (tokenErr, tokenRes) {
         if (tokenErr) {
           console.log(tokenErr, tokenRes);
-          res.status(500).json('Error from Singular Token', tokenErr);
+          res.status(500).json('Error from Singular Token');
         } else {
           // Sample call to get loggedin user info
           // Access token should be kept in your database
           var accessToken = tokenRes.body.access_token;
           //console.log(accessToken);
           superagent
-          .get(SINGULAR_API_SERVER + '/users/me')
+          .get(SINGULAR_SERVER + '/apiv1/users/me')
           .set('Authorization', 'Bearer ' + accessToken)
           .end(function(userErr, userRes) {
             //console.log(userRes.body);
-            res.json(userRes.body);
+            res.json('Your name is ' + userRes.body.name + ' with email address ' + userRes.body.email);
           });
         }
       });
