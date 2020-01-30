87743a2ca0d62dccef24870c927444e5c4248c6f
Thomas Molden <38147019+tmSingular@users.noreply.github.com>
2020-01-30 13:03:47 +0100

    Update router.js

--- server/router.js
+++ server/router.js
@@ -2,8 +2,8 @@ var superagent = require('superagent');
 
 var SINGULAR_SERVER = 'https://app.singular.live';
 //var SINGULAR_SERVER = 'http://localhost:3000';
-var CLIENT_ID = '[YOUR_CLIENT_ID]';
-var CLIENT_SECRET = '[YOUR_CLIENT_SECRET]';
+var CLIENT_ID = '472ea7a03e788befb6454f5a2c462eff73f3318b5217e576a368c4e3d93e41ed2e805da9c47d0160015dccb0389ac7c4'; // '[ENTER_YOUR_CLIENT_ID]'
+var CLIENT_SECRET = '6e463f8529f733c2c7b7abd14a7976ba1255294db77c2574ddbb71f4e0568fd9dda4b3abb04929cbfb4683b32361ee8d'; // '[ENTER_YOUR_CLIENT_SECRET]'
 
 module.exports = function (app) {
 
