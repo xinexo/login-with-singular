6b016040e40c1fb1f1daff609cff5105a8530986
t <hello@singular.live>
2020-02-16 20:28:27 +0700

    improvement: redirect on logoug

--- server/router.js
+++ server/router.js
@@ -4,6 +4,7 @@
 
 var superagent = require('superagent');
 
+//var SINGULAR_SERVER = 'http://localhost:3000';
 var SINGULAR_SERVER = 'https://beta.singular.live';
 var CLIENT_ID = '472ea7a03e788befb6454f5a2c462eff73f3318b5217e576a368c4e3d93e41ed2e805da9c47d0160015dccb0389ac7c4'; // '[ENTER_YOUR_CLIENT_ID]';
 var CLIENT_SECRET = '672b220b1becaabc1911e4d79e6df9030422e73c5ee4dde36e6dd367801dedac102480a9d1b3d7551ac49070b069e751'; // '[ENTER_YOUR_CLIENT_SECRET]';
@@ -20,6 +21,10 @@ module.exports = function (app) {
 
   // Callback
   app.get('/callback', function (req, res) {
+    if (req.query.action && req.query.action == 'logout') {
+      res.redirect('/');
+      return;
+    }
     if (!req.query.code) {
       res.status(400).json('No code provided');
       return;
@@ -42,7 +47,8 @@ module.exports = function (app) {
           superagent.get(SINGULAR_SERVER + '/apiv1/users/me').set('Authorization', 'Bearer ' + accessToken).end(function(userErr, userRes) {
             res.render('demo', {
               user: userRes.body,
-              singularUrlBase: SINGULAR_SERVER
+              singularUrlBase: SINGULAR_SERVER,
+              singularLogoutEndpoint: SINGULAR_SERVER + '/users/logout?client_id=' + CLIENT_ID
             });
           });
         }
